5c4ed99402c340a58894ceaceb61ae0483f76ac2
conan080262 <codenon@hotmail.com>
2019-04-05 14:54:58 +0700

    ADD Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,33 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAj1qQ8hRxOtfeEdVF1r2bY63BkCxsFU8U',
+  authDomain: 'fbionic21.firebaseapp.com',
+  databaseURL: 'https://fbionic21.firebaseio.com',
+  projectId: 'fbionic21',
+  storageBucket: 'fbionic21.appspot.com',
+  messagingSenderId: '487652028654'
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AngularFireDatabase
   ],
   bootstrap: [AppComponent]
 })
